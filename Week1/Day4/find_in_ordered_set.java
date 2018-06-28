cf314c6bc7037ccf42422dd0cf4e649809b29564
Raghavendra-Gali <gargvd0@gmail.com>
2018-06-28 16:51:03 +0530

    Week1 Day4 Programs Problem3

---
+++ Week1/Day4/find_in_ordered_set.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class find_in_ordered_set{
+	public static boolean binarySearch(int [] a,int num){
+		int low=0;
+		int high=a.length;
+		while(low<=high){
+			int mid=(low+high)/2;
+			if(a[mid]==num){
+				return true;
+			}
+			else if (a[mid]>num){
+				high=mid-1;
+			}
+			else{
+				low=mid+1;
+			}
+		}
+		return false;
+	}
+	public static void main(String[] args) {
+		int [] a={1,2,3,4,5,6,7,8};
+		System.out.println(binarySearch(a,8));
+	}
+}
