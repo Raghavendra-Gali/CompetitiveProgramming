d710f571a85f5e426631a4c144000c67335d58b6
Raghavendra Gali <gargvd0@gmail.com>
2018-07-09 09:39:51 +0530

    Updated The Program

--- Week2/Day6/in_place.py
+++ Week2/Day6/in_place.py
@@ -1,30 +1,21 @@
 import random
-import time
-def shuffle(the_list):
 
-    # Shuffle the input in place
-    i=0
-    random_numbers=[]
-    start=time.time()
-    while i<len(the_list):
-        r=random.randint(0,len(the_list)-1)
-        if r not in random_numbers:
-            random_numbers.append(r)
-            i+=1
-    print(time.time()-start)
-    print(random_numbers)
-    final_random_numbers=[]
-    the_temp=the_list[:]
-    the_list=[]
-    print("The temp",the_temp)
-    for i in range(len(the_list)):
-        r=random_numbers[i]
-        the_list.append(the_temp[r])   
+def inplace_shuffle(the_list):
+
+	# Shuffle the input in place
+	if len(the_list)<1:
+		return []
+	if(len(the_list)>1):
+		for i in range(len(the_list)):
+			j = random.randint(i,len(the_list)-1)
+			if i!=j:
+				the_list[i],the_list[j] = the_list[j],the_list[i]
+
 
 
 sample_list = [1, 2, 3, 4, 5]
-print('Sample list:', sample_list)
+print 'Sample list:', sample_list
 
-print('Shuffling sample list...')
+print 'Shuffling sample list...'
 shuffle(sample_list)
-print(sample_list)
+print sample_list
